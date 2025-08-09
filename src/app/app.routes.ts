f28cee32f10b2ef3368e9b66ea50f2be5f004716
imcewen <iwm2002@gmail.com>
2025-08-09 16:24:11 -0400

    Add a home screen and 404 screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './game-component/game-component';
+import { HomeComponent } from './home-component/home-component';
+import { NotFoundComponent } from './not-found-component/not-found-component';
 
 export const routes: Routes = [
-	{ path: '', component: GameComponent }
+	{ path: '', component: HomeComponent },
+	{ path: 'game', component: GameComponent },
+	{ path: '**', component: NotFoundComponent }
 ];
