e24a3d8f269a8c5b156186006266a01669cac919
imcewen <iwm2002@gmail.com>
2025-09-03 22:35:06 -0400

    feature(logout): Add logout and login button to header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { ToastComponent } from "./toast-component/toast-component";
+import { AuthService } from './services/auth-service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,17 @@ import { ToastComponent } from "./toast-component/toast-component";
   styleUrl: './app.css'
 })
 export class App {
+	constructor(
+		protected router: Router, 
+		private authService: AuthService
+	) { }
 
+	protected logoutUser() { 
+		this.authService.logout();
+		this.router.navigateByUrl("/")
+	}
+
+	protected getCurrentUser(): string | null {
+		return this.authService.getCurrentUsername();
+	}
 }
