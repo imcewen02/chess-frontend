6c9c35a2f94f8cfe6b22a090f47749a428cf46df
imcewen <iwm2002@gmail.com>
2025-09-25 23:33:16 -0400

    order ranks and files correctly on UI

--- src/app/game-component/game-component.ts
+++ src/app/game-component/game-component.ts
@@ -5,7 +5,6 @@ import { AccountService } from '../services/account-service';
 import { Game } from '../models/game';
 import { Position } from '../models/position';
 import { Color } from '../models/pieces';
-import { Board } from '../models/board';
 
 @Component({
 	selector: 'app-game-component',
@@ -14,15 +13,17 @@ import { Board } from '../models/board';
 	styleUrl: './game-component.css'
 })
 export class GameComponent {
-	private static readonly EMPTY_GAME: Game = {
-		uuid: "",
-		whitePlayer: null!,
-		blackPlayer: null!,
-		board: new Board(null),
-		currentTurn: Color.White
-	};
+	protected game = computed<Game | null>(() => { return this.gameService.activeGame() ? this.gameService.activeGame()! : null });
 
-	protected game = computed<Game>(() => { return this.gameService.activeGame() ? this.gameService.activeGame()! : GameComponent.EMPTY_GAME });
+	protected ranks = computed<number[]>(() => { 
+		if (this.game() == null) return [];
+		return this.getUsersColor() == Color.White ? [...this.game()!.board.ranks].reverse() : this.game()!.board.ranks 
+	});
+
+	protected files = computed<string[]>(() => { 
+		if (this.game() == null) return [];
+		return this.getUsersColor() == Color.White ? this.game()!.board.files : [...this.game()!.board.files].reverse() 
+	});
 
 	protected selectedPosition: Position | null = null;
 
@@ -42,7 +43,7 @@ export class GameComponent {
 		if (this.isPositionInSelectedPiecesLegalMoves(position)) {
 			this.gameService.movePiece(this.selectedPosition!, position);
 			this.selectedPosition = null;
-		} else if (this.game().board.getPieceAtPosition(position)?.color == this.getUsersColor()) {
+		} else if (this.game()!.board.getPieceAtPosition(position)?.color == this.getUsersColor()) {
 			this.selectedPosition = position;
 		} else {
 			this.selectedPosition = null;
@@ -50,19 +51,25 @@ export class GameComponent {
 	}
 
 	protected isUsersTurn(): boolean {
+		if (this.game() == null) return false;
+
 		const usersName = this.accountService.loggedInAccount()?.username;
-		return this.game().currentTurn == Color.White ? this.game().whitePlayer.username == usersName : this.game().blackPlayer.username == usersName;
+		return this.game()!.currentTurn == Color.White ? this.game()!.whitePlayer.username == usersName : this.game()!.blackPlayer.username == usersName;
 	}
 
-	protected getUsersColor(): Color {
+	protected getUsersColor(): Color | null {
+		if (this.game() == null) return null;
+
 		const usersName = this.accountService.loggedInAccount()?.username;
-		return this.game().whitePlayer.username == usersName ? Color.White : Color.Black;
+		return this.game()!.whitePlayer.username == usersName ? Color.White : Color.Black;
 	}
 
 	protected isPositionInSelectedPiecesLegalMoves(position: Position): boolean {
-		const pieceAtSelectedPosition = this.selectedPosition ? this.game().board.getPieceAtPosition(this.selectedPosition) : null;
+		if (this.game() == null) return false;
+
+		const pieceAtSelectedPosition = this.selectedPosition ? this.game()!.board.getPieceAtPosition(this.selectedPosition) : null;
 		if (!pieceAtSelectedPosition) return false;
 
-		return pieceAtSelectedPosition.getAvailableMoves(this.game().board, true)!.some(move => move.rank == position.rank && move.file == position.file)
+		return pieceAtSelectedPosition.getAvailableMoves(this.game()!.board, true)!.some(move => move.rank == position.rank && move.file == position.file)
 	}
 }
